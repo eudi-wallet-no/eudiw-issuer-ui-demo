b5553ab6cd42d40ac83c0a5d2af9c9fdfbe174f0
Randi Øyri <randi.oyri@digdir.no>
2026-01-21 13:05:54 +0100

    EUW-1110: Støtte for bilde som datatype i bevisdefinisjon

--- src/main/java/no/idporten/eudiw/issuer/ui/demo/byob/CredentialDefinitionFactory.java
+++ src/main/java/no/idporten/eudiw/issuer/ui/demo/byob/CredentialDefinitionFactory.java
@@ -3,52 +3,53 @@ package no.idporten.eudiw.issuer.ui.demo.byob;
 import no.idporten.eudiw.issuer.ui.demo.byob.model.*;
 
 import java.util.List;
-import java.util.Map;
 
 public class CredentialDefinitionFactory {
 
     public static CredentialDefinition empty() {
         return new CredentialDefinition(
-            "your-credential-configuration-name (automatically generated)",
-            List.of(
-                new ExampleCredentialData("family_name", "Normann"),
-                new ExampleCredentialData("given_name", "Kari")
-            ),
-            new CredentialMetadata(
+                "your-credential-configuration-name (automatically generated)",
                 List.of(
-                    new Display(
-                        "test",
-                        "no",
-                        null,
-                        null
-                    )
+                        new ExampleCredentialData("family_name", "Normann"),
+                        new ExampleCredentialData("given_name", "Kari")
                 ),
-                List.of(
-                    new Claim(
-                        "family_name",
-                        true,
+                new CredentialMetadata(
                         List.of(
-                            new Display(
-                                "Etternavn",
-                                "no",
-                                null,
-                                null
-                            )
+                                new Display(
+                                        "Namn på mitt bevis",
+                                        "no",
+                                        null,
+                                        null
+                                )
+                        ),
+                        List.of(
+                                new Claim(
+                                        "family_name",
+                                        "string",
+                                        true,
+                                        List.of(
+                                                new Display(
+                                                        "Etternavn",
+                                                        "no",
+                                                        null,
+                                                        null
+                                                )
+                                        )
+                                ),
+                                new Claim(
+                                        "given_name",
+                                        "string",
+                                        true,
+                                        List.of(new Display(
+                                                        "Fornavn",
+                                                        "no",
+                                                        null,
+                                                        null
+                                                )
+                                        )
+                                )
                         )
-                    ),
-                        new Claim(
-                            "given_name",
-                            true,
-                            List.of(new Display(
-                                "Fornavn",
-                                "no",
-                                null,
-                                null
-                           )
-                        )
-                    )
                 )
-            )
         );
     }
 }
