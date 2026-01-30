0276b2c73764f204fba6498a4a2caedfbdc48237
Thomas Reppesgård <thr@digdir.no>
2026-01-30 15:43:16 +0100

    Legge til støtte for authorization code grant

--- src/main/java/no/idporten/eudiw/bevisgenerator/integration/issuerserver/domain/AuthorizationCodeGrant.java
+++ src/main/java/no/idporten/eudiw/bevisgenerator/integration/issuerserver/domain/AuthorizationCodeGrant.java
@@ -5,10 +5,11 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.net.URI;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public record Grants(
-        @JsonProperty("urn:ietf:params:oauth:grant-type:pre-authorized_code") PreAuthorizedCodeGrant preAuthorizedCodeGrant,
-        @JsonProperty("authorization_code") AuthorizationCodeGrant authorizationCodeGrant) {
+public record AuthorizationCodeGrant(
+        @JsonProperty("issuer_state") String issuerState,
+        @JsonProperty("authorization_server") URI authorizationServer) {
 }
-
