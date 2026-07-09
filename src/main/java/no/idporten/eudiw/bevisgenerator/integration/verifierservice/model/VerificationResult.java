431e3a3fbeb03bb4da385541c761934ae45689a2
Erik Teien Jarem <erik.teien.jarem@digdir.no>
2026-07-09 22:18:11 +0200

    Fixed verificationresult

--- src/main/java/no/idporten/eudiw/bevisgenerator/integration/verifierservice/model/VerificationResult.java
+++ src/main/java/no/idporten/eudiw/bevisgenerator/integration/verifierservice/model/VerificationResult.java
@@ -2,11 +2,18 @@ package no.idporten.eudiw.bevisgenerator.integration.verifierservice.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import tools.jackson.databind.JsonNode;
+
+import java.util.List;
+import java.util.Map;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record VerificationResult(
         @JsonProperty("verifier_transaction_id") String verifierTransactionId,
-        @JsonProperty("credentials") JsonNode credentials
+        @JsonProperty("credentials") Map<String, List<CredentialPresentation>> credentials
 ) {
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record CredentialPresentation(
+            @JsonProperty("claims") Map<String, Object> claims
+    ) {
+    }
 }
