5391fa527d82613e47c8d6d7ccaa3db3354930f1
Erik Teien Jarem <erik.teien.jarem@digdir.no>
2026-07-09 22:14:08 +0200

    Issues

--- src/main/java/no/idporten/eudiw/bevisgenerator/integration/verifierservice/model/VerificationResult.java
+++ src/main/java/no/idporten/eudiw/bevisgenerator/integration/verifierservice/model/VerificationResult.java
@@ -1,4 +1,12 @@
 package no.idporten.eudiw.bevisgenerator.integration.verifierservice.model;
 
-public record VerificationResult(String result) {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import tools.jackson.databind.JsonNode;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record VerificationResult(
+        @JsonProperty("verifier_transaction_id") String verifierTransactionId,
+        @JsonProperty("credentials") JsonNode credentials
+) {
 }
